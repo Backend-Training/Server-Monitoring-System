29d0214959779e0dc991a9af76c1cd758a380f5a
amrbadran <badran.amr2003@gmail.com>
2025-09-25 10:06:52 +0300

    Collection Service : Add DTOs For Deserialize Process

---
+++ ServerMonitoringSystem.Collector/Configuration/ServerStatisticsConfig.cs
@@ -0,0 +1,7 @@
+namespace ServerMonitoringSystem.Collector.Configuration;
+
+public class ServerStatisticsConfig
+{
+    public double SamplingIntervalSeconds { get; set; }
+    public string ServerIdentifier { get; set; }
+}
