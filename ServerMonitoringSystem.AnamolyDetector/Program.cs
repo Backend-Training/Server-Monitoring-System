2d83b9b5db554638562f1159df845bbc8885aa67
amrbadran <badran.amr2003@gmail.com>
2025-09-26 22:13:30 +0300

    AnamolyDetector Service : Build Program.cs and Test Communcation & Persitence

---
+++ ServerMonitoringSystem.AnamolyDetector/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+using MongoDB.Driver;
+using RabbitMQ;
+using RabbitMQ.Configuration;
+using ServerMonitoringSystem.AnamolyDetector.Models;
+using ServerMonitoringSystem.AnamolyDetector.MongoDB;
+using ServerMonitoringSystem.AnamolyDetector.Persitence;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+var consumer = await RabbitMqConsumer.CreateAsync(
+    new RabbitMqSettings(),
+    exchangeName: "server_stats",               
+    queueName: "server_stats_queue",          
+    routingKey: "ServerStatistics.*"           
+);
+
+IPersitence persitenceService = new MongoDbPersitence(
+    databaseName:configuration["MongoDB:Database"],
+    collectionName:configuration["MongoDB:DatabaseCollection"],
+    client: new MongoClient(configuration["MongoDB:MongoURI"])
+);
+
+await consumer.StartAsync(async (message, routingKey) =>
+{
+    var identifer = routingKey.Split(".")[1];
+    var serverStatistics = JsonSerializer.Deserialize<ServerStatistics>(message);
+    serverStatistics.ServerIdentifier = identifer;
+    
+    await persitenceService.Save(serverStatistics);
+});
