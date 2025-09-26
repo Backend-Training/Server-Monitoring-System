4aedef9b41389308ac186814e07551a86d4beb4c
amrbadran <badran.amr2003@gmail.com>
2025-09-26 23:34:03 +0300

    AnamolyDetector Service : Add SignalR Alerting Service

--- ServerMonitoringSystem.AnamolyDetector/Signaling/Interfaces/ISignal.cs
+++ ServerMonitoringSystem.AnamolyDetector/Signaling/Interfaces/ISignal.cs
@@ -3,4 +3,7 @@ namespace ServerMonitoringSystem.AnamolyDetector.Signaling.Interfaces;
 public interface ISignal
 {
     public Task Invoke(string signal, string message);
+    public Task Start();
+    public Task Stop();
+
 }
