81f44c811bddd6500d3d0f8ecea8189cddc21682
amrbadran <badran.amr2003@gmail.com>
2025-09-27 19:44:52 +0300

    Consumer Service : Implement Program.cs

---
+++ ServerMonitoringSystem.Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+using ServerMonitoringSystem.Consumer.Signaling.Interfaces;
+using ServerMonitoringSystem.Consumer.Signaling.SignalR;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+
+ISignalConsumer consumer = new SignalRConsumer(
+    new HubConnectionBuilder()
+        .WithUrl(configuration["SignalRConfig:SignalRUrl"])
+        .WithAutomaticReconnect()
+        .Build()
+);  
+
+await consumer.Start();
+
+consumer.Register("ReceiveAlert", (message) =>
+{
+    Console.WriteLine($"{DateTime.Now:HH:mm:ss}  {message}");
+});
+
+await Task.Delay(-1); // Keep it alive
