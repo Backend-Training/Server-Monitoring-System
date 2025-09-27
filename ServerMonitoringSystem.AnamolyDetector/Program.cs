a57b496a7660d5638536b73c22c1f73f98dc568f
amrbadran <badran.amr2003@gmail.com>
2025-09-27 17:19:30 +0300

    AnamolyDetection Service : Program.cs Implemention Of SignalR Logic

--- ServerMonitoringSystem.AnamolyDetector/Program.cs
+++ ServerMonitoringSystem.AnamolyDetector/Program.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System.Text.Json;
+using Microsoft.AspNetCore.SignalR.Client;
 using MongoDB.Driver;
 using RabbitMQ;
 using RabbitMQ.Configuration;
 using ServerMonitoringSystem.AnamolyDetector.Models;
 using ServerMonitoringSystem.AnamolyDetector.MongoDB;
 using ServerMonitoringSystem.AnamolyDetector.Persitence;
+using ServerMonitoringSystem.AnamolyDetector.Services;
+using ServerMonitoringSystem.AnamolyDetector.Services.Interfaces;
+using ServerMonitoringSystem.AnamolyDetector.Signaling.Interfaces;
+using ServerMonitoringSystem.AnamolyDetector.Signaling.SignalR;
 
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
@@ -13,15 +18,31 @@ var configuration = new ConfigurationBuilder()
     .Build();
 var consumer = await RabbitMqConsumer.CreateAsync(
     new RabbitMqSettings(),
-    exchangeName: "server_stats",               
-    queueName: "server_stats_queue",          
-    routingKey: "ServerStatistics.*"           
+    exchangeName: "server_stats",
+    queueName: "server_stats_queue",
+    routingKey: "ServerStatistics.*"
 );
 
-IPersitence persitenceService = new MongoDbPersitence(
-    databaseName:configuration["MongoDB:Database"],
-    collectionName:configuration["MongoDB:DatabaseCollection"],
+IRepository repositoryService = new MongoDbRepository(
+    databaseName: configuration["MongoDB:Database"],
+    collectionName: configuration["MongoDB:DatabaseCollection"],
     client: new MongoClient(configuration["MongoDB:MongoURI"])
+);
+
+ISignal signalR = new SignalRAlerting(
+    new HubConnectionBuilder()
+        .WithUrl(configuration["SignalRConfig:SignalRUrl"])
+        .WithAutomaticReconnect()
+        .Build()
+);
+IAlert anamolyAlertService = new AnamolyAlertService(
+    repository: repositoryService,
+    signal: signalR,
+    anomalyDetectionConfig: configuration["AnomalyDetectionConfig"]
+);
+IAlert highUsageAlertService = new HighUsageAlertService(
+    signal: signalR,
+    highUsageThersholdConfig: configuration["AnomalyDetectionConfig"]
 );
 
 await consumer.StartAsync(async (message, routingKey) =>
@@ -30,5 +51,8 @@ await consumer.StartAsync(async (message, routingKey) =>
     var serverStatistics = JsonSerializer.Deserialize<ServerStatistics>(message);
     serverStatistics.ServerIdentifier = identifer;
     
-    await persitenceService.Save(serverStatistics);
+    await anamolyAlertService.SendAlert(serverStatistics);
+    await highUsageAlertService.SendAlert(serverStatistics);
+
+    await repositoryService.Save(serverStatistics);
 });
